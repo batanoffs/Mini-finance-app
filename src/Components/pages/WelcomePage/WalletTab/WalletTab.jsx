057868f8ed6887419260a409072c212eb3ca9340
batanoffs <batanoff.s@protonmail.com>
2024-02-24 00:25:07 +0200

    added contacts to wallet

--- src/Components/pages/WelcomePage/WalletTab/WalletTab.jsx
+++ src/Components/pages/WelcomePage/WalletTab/WalletTab.jsx
@@ -1,17 +1,19 @@
 import { BankingActionButtons } from "../assets/BankingActionsButtons/BankingActionButtons";
 import { LastTransactions } from "../assets/LastTransactions";
-import { TableTransactions } from './assets/TableTransactions';
-import styles from "../welcome-page-layout.module.css"
-
+import { TableTransactions } from "./assets/TableTransactions";
+import { ContactInfo } from "../assets/ContactInfo";
+import styles from "../welcome-page-layout.module.css";
 
 export const WalletTab = () => {
     return (
         <div className={styles.contentContainer}>
             <main className={styles.bentoFillColumn}>
-                <TableTransactions/>
-            </main>
-            <aside className={styles.bentoSideColumn} >
+                <TableTransactions />
                 <LastTransactions />
+
+            </main>
+            <aside className={styles.bentoSideColumn}>
+                <ContactInfo />
                 <BankingActionButtons />
             </aside>
         </div>
