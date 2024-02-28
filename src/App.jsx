084e92794b0f2704452cd38fbfa016f88d946a8b
batanoffs <batanoff.s@protonmail.com>
2024-02-28 13:25:47 +0200

    fix routing in 404 error page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import { Header } from "./Components/pages/Header/Header";
 import { Login } from "./Components/pages/LoginPage/Login";
@@ -9,6 +9,10 @@ import { Register } from "./Components/pages/RegisterPage/Register";
 import { Result } from "antd";
 
 function App() {
+    let navigate = useNavigate();
+    let goBack = () => {
+        navigate(-1);
+    };
     return (
         <AuthProvider>
             <Header />
@@ -18,30 +22,28 @@ function App() {
                         path="*"
                         element={
                             <Result
-                                style={{ alignSelf: "center", margin: "0 auto" }}
+                                style={{
+                                    alignSelf: "center",
+                                    margin: "0 auto",
+                                }}
                                 status="404"
                                 title="Грешка 404, не е намерена страница."
                                 subTitle="Страницата, която търсите не съществува."
                                 extra={
-                                    <Link
-                                        to="/"
-                                        className="button-primary">
-                                        Начална страница
-                                    </Link>
-                                    }
+                                    <button
+                                        onClick={goBack}
+                                        className="button-primary"
+                                    >
+                                        Назад
+                                    </button>
+                                }
                             />
                         }
                     />
                     <Route path="/" element={<Home />} />
                     <Route path="/login" element={<Login />} />
-                    <Route
-                        path="/register/*"
-                        element={<Register />}
-                    />
-                    <Route
-                        path="/dashboard/*"
-                        element={<WelcomePage />}
-                    />
+                    <Route path="/register/*" element={<Register />} />
+                    <Route path="/dashboard/*" element={<WelcomePage />} />
                 </Routes>
             </main>
             <Footer />
