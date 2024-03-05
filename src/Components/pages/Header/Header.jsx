0952259f8c344cdddf4e2dbb003228c80e36616a
batanoffs <batanoff.s@protonmail.com>
2024-03-05 09:43:20 +0200

    improved logic header dropdown homepage

--- src/Components/pages/Header/Header.jsx
+++ src/Components/pages/Header/Header.jsx
@@ -1,17 +1,75 @@
 import { AuthContext } from "../../../contexts/AuthContext";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars} from "@fortawesome/free-solid-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { Notifications } from "./assets/Notifications/Notifications";
 import { ProfileDropdown } from "./assets/Profile/ProfileDropdown";
 import styles from "./site-header.module.css";
 
-// import logo from "../../../images/logo/logo4.svg";
-
 export const Header = () => {
-    const { isAuthenticated, picture, onLogoutHandler, name } = useContext(AuthContext);
+    const { isAuthenticated, picture, onLogoutHandler, name } =
+        useContext(AuthContext);
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 600);
     const navigate = useNavigate();
+    const showIcon = isMobile ? (
+        <FontAwesomeIcon className={styles.headerDropdownIcon} icon={faBars} />
+    ) : null;
+
+    const showButtons = isMobile ? (
+        <div
+            to="#"
+            role="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+            className={styles.dropdownToggle}
+        >
+            <ul className={styles.dropdownMenu}>
+                <li>
+                    <Link className={styles.dropdownItem} to="/login">
+                        Вход
+                    </Link>
+                </li>
+                <li>
+                    <Link className={styles.dropdownItem} to="/register">
+                        Нов Акаунт
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    ) : (
+        <>
+            <Link
+                to="/login"
+                className={styles.buttonLogin}
+                type="button"
+                style={{
+                    borderBottomRightRadius: "0px",
+                    borderTopRightRadius: "0px",
+                }}
+            >
+                Вход
+            </Link>
+            <Link
+                to="/register"
+                className={styles.buttonRegister}
+                name="register"
+                type="button"
+            >
+                Нов Акаунт
+            </Link>
+        </>
+    );
+    const handleResize = () => {
+        setIsMobile(window.innerWidth < 768);
+    };
+
+    useEffect(() => {
+        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
 
     const onRedirect = () => {
         if (isAuthenticated()) {
@@ -31,72 +89,18 @@ export const Header = () => {
                     className={styles.logo}
                 />
             </div>
+
             {!isAuthenticated() && (
-                <>
-                    <div className={styles.headerMobileDropdown}>
-                        <FontAwesomeIcon
-                            className={styles.headerDropdownIcon}
-                            icon={faBars}
-                        />
-                        <div
-                            to="#"
-                            role="button"
-                            data-bs-toggle="dropdown"
-                            aria-expanded="false"
-                            className={styles.dropdownToggle}
-                        >
-                            <ul className={styles.dropdownMenu}>
-                                <li>
-                                    <Link
-                                        className={styles.dropdownItem}
-                                        to="/login"
-                                    >
-                                        Вход
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link
-                                        className={styles.dropdownItem}
-                                        to="/register"
-                                    >
-                                        Нов Акаунт
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className={styles.headerButtons}>
-                        <Link
-                            to="/login"
-                            className={styles.buttonLogin}
-                            type="button"
-                            style={{
-                                borderBottomRightRadius: "0px",
-                                borderTopRightRadius: "0px",
-                            }}
-                        >
-                            Вход
-                        </Link>
-                        <Link
-                            to="/register"
-                            className={styles.buttonRegister}
-                            name="register"
-                            type="button"
-                        >
-                            Нов Акаунт
-                        </Link>
-                    </div>
-                </>
+                <div className={isMobile ? styles.headerMobileDropdown : styles.headerButtons}>
+                    {showIcon}
+                    {showButtons}
+                </div>
             )}
 
             {isAuthenticated() && (
                 <div className={styles.headerDropdownContainer}>
                     <Notifications />
-                    <ProfileDropdown
-                        onLogoutHandler={onLogoutHandler}
-                        name={name}
-                        picture={picture}
-                    />
+                    <ProfileDropdown onLogoutHandler={onLogoutHandler} name={name} picture={picture} />
                 </div>
             )}
         </header>
