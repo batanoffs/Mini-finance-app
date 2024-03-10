ee12c4cb6f3d83077e6799391d8af55b3f5fed61
batanoffs <batanoff.s@protonmail.com>
2024-03-10 05:07:34 +0200

    updated transactions service

--- src/services/userTransactionService.js
+++ src/services/userTransactionService.js
@@ -27,6 +27,37 @@ const send = async (fullname, amount, type, sender, token) => {
                     transaction_type: type,
                 },
             },
+            {
+                operationType: "ADD_RELATION",
+                table: "MoneyTransactions",
+                opResultId: "setReciver",
+                payload: {
+                    parentObject: {
+                        ___ref: true,
+                        opResultId: "newEntry",
+                        propName: "objectId",
+                    },
+                    relationColumn: "receiver",
+                    unconditional: {
+                        ___ref: true,
+                        opResultId: "findReciever",
+                    },
+                },
+            },
+            {
+                operationType: "ADD_RELATION",
+                table: "MoneyTransactions",
+                opResultId: "setSender",
+                payload: {
+                    parentObject: {
+                        ___ref: true,
+                        opResultId: "newEntry",
+                        propName: "objectId",
+                    },
+                    relationColumn: "sender",
+                    unconditional: [sender],
+                },
+            },
         ],
     };
 
@@ -37,47 +68,73 @@ const send = async (fullname, amount, type, sender, token) => {
         token
     );
     console.log("1", response);
-
-    if (response.success) {
-        const body2 = {
-            isolationLevelEnum: "READ_COMMITTED",
-            operations: [
-                {
-                    operationType: "ADD_RELATION",
-                    table: "MoneyTransactions",
-                    opResultId: "setReciver",
-                    payload: {
-                        parentObject: response.results.newEntry.result.objectId,
-                        relationColumn: "receiver",
-                        unconditional: [
-                            response.results.findReciever.result[0].objectId,
-                        ],
-                    },
-                },
-                {
-                    operationType: "ADD_RELATION",
-                    table: "MoneyTransactions",
-                    opResultId: "setSender",
-                    payload: {
-                        parentObject: response.results.newEntry.result.objectId,
-                        relationColumn: "sender",
-                        unconditional: [sender],
-                    },
-                },
-            ],
-        };
-
-        const response2 = await request.post(
-            `${baseURL}${endpoints.transactions}`,
-            body2
-        );
-        return response2;
-    }
 };
 
 export const transactions = {
     send,
 };
+
+
+    // const body = {
+    //     isolationLevelEnum: "READ_COMMITTED",
+    //     operations: [
+    //         {
+    //             operationType: "FIND",
+    //             table: "UserData",
+    //             opResultId: "findReciever",
+    //             payload: {
+    //                 whereClause: `fullName = '${fullname}'`,
+    //             },
+    //         },
+    //         {
+    //             operationType: "CREATE",
+    //             table: "MoneyTransactions",
+    //             opResultId: "newEntry",
+    //             payload: {
+    //                 amount: amount,
+    //                 transaction_type: type,
+    //             },
+    //         },
+    //     ],
+    // };
+
+    
+
+    // if (response.success) {
+    //     const body2 = {
+    //         isolationLevelEnum: "READ_COMMITTED",
+    //         operations: [
+    //             {
+    //                 operationType: "ADD_RELATION",
+    //                 table: "MoneyTransactions",
+    //                 opResultId: "setReciver",
+    //                 payload: {
+    //                     parentObject: response.results.newEntry.result.objectId,
+    //                     relationColumn: "receiver",
+    //                     unconditional: [
+    //                         response.results.findReciever.result[0].objectId,
+    //                     ],
+    //                 },
+    //             },
+    //             {
+    //                 operationType: "ADD_RELATION",
+    //                 table: "MoneyTransactions",
+    //                 opResultId: "setSender",
+    //                 payload: {
+    //                     parentObject: response.results.newEntry.result.objectId,
+    //                     relationColumn: "sender",
+    //                     unconditional: [sender],
+    //                 },
+    //             },
+    //         ],
+    //     };
+
+    //     const response2 = await request.post(
+    //         `${baseURL}${endpoints.transactions}`,
+    //         body2
+    //     );
+    //     return response2;
+    // }
 
 // {
 //     "isolationLevelEnum": "READ_COMMITTED",
@@ -87,7 +144,7 @@ export const transactions = {
 //             "table": "UserData",
 //             "opResultId": "findReciever",
 //             "payload": {
-//                 "whereClause": "fullName = 'Иво Киров'"
+//                 "whereClause": "fullName = 'Tommy Smith'"
 //             }
 //         },
 //         {
@@ -95,39 +152,42 @@ export const transactions = {
 //             "table": "MoneyTransactions",
 //             "opResultId": "newEntry",
 //             "payload": {
-//                 "amount": 6126,
+//                 "amount": 12125,
 //                 "transaction_type": "+"
 //             }
 //         },
 //         {
-//             "operationType": "FIND",
+//             "operationType": "ADD_RELATION",
 //             "table": "MoneyTransactions",
-//             "opResultId": "transactionId",
-//             "whereClause" : "where clause",
+//             "opResultId": "setReciver",
 //             "payload": {
-//                 "objectId": {
+//                 "parentObject": {
 //                     "___ref": true,
 //                     "opResultId": "newEntry",
-//                     "resultIndex": 0,
 //                     "propName": "objectId"
+//                 },
+//                 "relationColumn": "receiver",
+//                 "unconditional": {
+//                     "___ref": true,
+//                     "opResultId": "findReciever"
 //                 }
 //             }
 //         },
 //         {
-//              "operationType": "ADD_RELATION",
-//              "table": "MoneyTransactions",
-//              "opResultId": "setReciver",
-//              "payload": {
-//                  "parentObject": {
-//                      "___ref": true,
-//                      "opResultId": "findReciever",
-//                      "resultIndex": 0,
-//                      "propName": "objectId"
-//                  },
-//                  "relationColumn": "receiver",
-//                  "unconditional": {
-//                      "___ref": true,
-//                      "opResultId": "newEntry"
+//             "operationType": "ADD_RELATION",
+//             "table": "MoneyTransactions",
+//             "opResultId": "setSender",
+//             "payload": {
+//                 "parentObject": {
+//                     "___ref": true,
+//                     "opResultId": "newEntry",
+//                     "propName": "objectId"
+//                 },
+//                 "relationColumn": "sender",
+//                 "unconditional": [
+//                     "983BC9D0-48C3-43F0-8B97-F5E4C6EF18A3"
+//                 ]
+//             }
 //         }
-//     }
+//     ]
 // }
