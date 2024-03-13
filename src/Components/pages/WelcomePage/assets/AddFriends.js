deda206dc8f9716b8ce00797f8fbb6d923310c7d
batanoffs <batanoff.s@protonmail.com>
2024-03-13 13:45:47 +0200

    updated addnumber module onFocus

--- src/Components/pages/WelcomePage/assets/AddFriends.js
+++ src/Components/pages/WelcomePage/assets/AddFriends.js
@@ -1,57 +1,70 @@
 import { useState, useContext } from "react";
 import { AuthContext } from "../../../../contexts/AuthContext";
 import { notifications } from "../../../../services/notificationService";
-import blocks from "../custom-block.module.css"
-import styles from "./addfriends.module.css"
+import blocks from "../custom-block.module.css";
+import styles from "./addfriends.module.css";
 
 export const AddFriends = () => {
-    const [number, setNumber] = useState('');
+    const [number, setNumber] = useState("");
     const [error, setError] = useState(false);
     const { userDataId, token } = useContext(AuthContext);
 
     const onChangeNumber = (e) => {
         setNumber(e.target.value);
-    }
+    };
 
     const onFocusClearErrorHandler = (e) => {
+        e.target.style.border = `1px solid var(--primary-hover-color)`;
+
         setError(false);
-    }
+    };
 
     const onSubmit = async (e) => {
         e.preventDefault();
-        if( !number ) {
+        if (!number) {
             setError(true);
             return;
         }
-        const response = await notifications.createNotification(number, null, "friend request", userDataId, token);
-        
-        if(response.success) {
+        const response = await notifications.createNotification(
+            number,
+            null,
+            "friend request",
+            userDataId,
+            token
+        );
+
+        if (response.success) {
             window.alert("Успешно изпратихте покана за приятелство");
         } else {
             window.alert("Няма такъв потребител");
         }
         setNumber("");
-    }
+    };
     return (
         <div className={`${blocks.customBlockContact}`}>
             <header>
                 <h5>Добави приятел</h5>
             </header>
             <form onSubmit={onSubmit} className={styles.friendsForm}>
-                { error ? <small style={{color: "red"}}>липсва телефонен номер</small> : null }
+                {error ? (
+                    <small style={{ color: "red" }}>
+                        липсва телефонен номер
+                    </small>
+                ) : null}
                 <input
                     type="text"
                     placeholder="телефон"
-                    required=""
+                    onBlur={(e) => {
+                        if (!number) {
+                            e.target.style.border = `1px solid transparent`;
+                        }
+                    }}
+                    required
                     value={number}
                     onChange={onChangeNumber}
                     onFocus={onFocusClearErrorHandler}
                 />
-                <input
-                    type="submit"
-                    className="custom-btn"
-                    value="Добави"
-                />
+                <input type="submit" className="custom-btn" value="Добави" />
             </form>
         </div>
     );
