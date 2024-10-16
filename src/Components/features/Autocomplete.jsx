7f8123f1176874d8c08fbfee528ae8cedf31b6b1
batanoffs <batanoff.s@protonmail.com>
2024-10-16 10:46:43 +0300

    fix import

--- src/Components/features/Autocomplete.jsx
+++ src/Components/features/Autocomplete.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-import './autocomplate.css'
+import './autocomplete.css'
 
 export const Autocomplete = (props) => {
     const [activeSuggestion, setActiveSuggestion] = useState(0)
