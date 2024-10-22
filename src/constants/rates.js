298ecb310e4dbbee9b6e2d83ffd4c944dc4eb3de
batanoffs <batanoff.s@protonmail.com>
2024-10-22 15:10:35 +0300

    add constant for rates

---
+++ src/constants/rates.js
@@ -0,0 +1,32 @@
+export const exchangeRates = {
+    USD: {
+        name: 'USD',
+        buy: 0,
+        sell: 0,
+        logo: 'https://res.cloudinary.com/dzh01qrmx/image/upload/v1729500809/united-states_rblegx.png',
+    },
+    GBP: {
+        name: 'GBP',
+        buy: 0,
+        sell: 0,
+        logo: 'https://res.cloudinary.com/dzh01qrmx/image/upload/v1729500566/xmct4dcgmfugpnnsdzpe.png',
+    },
+    EUR: {
+        name: 'EUR',
+        buy: 0,
+        sell: 0,
+        logo: 'https://res.cloudinary.com/dzh01qrmx/image/upload/v1729500566/lec08kknashadt8staca.png',
+    },
+    AUD: {
+        name: 'AUD',
+        buy: 0,
+        sell: 0,
+        logo: 'https://res.cloudinary.com/dzh01qrmx/image/upload/v1729500566/iyeizidmnwuungojoyrb.png',
+    },
+    SGD: {
+        name: 'SGD',
+        buy: 0,
+        sell: 0,
+        logo: 'https://res.cloudinary.com/dzh01qrmx/image/upload/v1729500566/kljvsxfskjfscptvmulr.png',
+    },
+}
