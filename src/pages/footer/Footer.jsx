fc6911af940271c72357328b080e98dd927633a8
batanoffs <batanoff.s@protonmail.com>
2024-10-23 14:44:10 +0300

    update text

--- src/pages/footer/Footer.jsx
+++ src/pages/footer/Footer.jsx
@@ -12,7 +12,7 @@ export const Footer = () => {
                 <footer className={styles.site_footer}>
                     <div className={styles.footer_container}>
                         <p className={styles.copyright_text}>
-                            All rights reserved &copy; Mini Finance Innovations 2030. Design by{' '}
+                            All rights reserved &copy; Mini Finance Innovations 2024. Programmed by{' '}
                             <a href="https://github.com/batanoffs">batanoffs.</a> Illustration by{' '}
                             <a href="https://icons8.com/illustrations/author/627444">Julia G</a>{' '}
                             from <a href="https://icons8.com/illustrations">Ouch!</a>
