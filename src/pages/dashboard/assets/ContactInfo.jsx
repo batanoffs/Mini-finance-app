3be3049baf3ec637c9b2117a21a784b041bcff19
batanoffs <batanoff.s@protonmail.com>
2024-10-30 11:30:55 +0200

    clean up

--- src/pages/dashboard/assets/ContactInfo.jsx
+++ src/pages/dashboard/assets/ContactInfo.jsx
@@ -1,4 +1,3 @@
-// import { Link } from "react-router-dom";
 import containers from './containers.module.css'
 
 export const ContactInfo = () => {
@@ -10,13 +9,6 @@ export const ContactInfo = () => {
                 <strong>Get in touch:</strong>
                 <a href="tel: 305-240-9671">(60) 305-240-9671</a>
             </p>
-
-            {/* <Link
-                    to="#"
-                    className="custom-btn"
-                >
-                    Start Chat
-                </Link> */}
         </div>
     )
 }
