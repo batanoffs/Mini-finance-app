057b560c66d5ed86e84df2822e2b242174225ed0
batanoffs <batanoff.s@protonmail.com>
2024-11-04 16:01:15 +0200

    update remove props to handle internally in each tab component

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,50 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
-import { useState } from 'react'
-import {
-    HelpCenterTab,
-    SettingsTab,
-    OverviewTab,
-    ProfileTab,
-    WalletTab,
-    Upgrade,
-    Sidebar,
-} from './tabs/index'
+import { HelpCenterTab, SettingsTab, OverviewTab, ProfileTab, WalletTab, Upgrade, Sidebar } from './tabs/index'
 
 export const Dashboard = () => {
-    const [showModal, setShowModal] = useState({
-        topUp: false,
-        send: false,
-        request: false,
-    })
-    const [userInput, setUserInput] = useState({ amount: '', friends: '' })
-
     return (
         <div className="main-wrapper">
             <Sidebar />
 
             <Routes>
-                <Route
-                    path="/overview"
-                    element={
-                        <OverviewTab
-                            showModal={showModal}
-                            userInput={userInput}
-                            setUserInput={setUserInput}
-                            setShowModal={setShowModal}
-                        />
-                    }
-                />
-                <Route
-                    path="/wallet"
-                    element={
-                        <WalletTab
-                            showModal={showModal}
-                            userInput={userInput}
-                            setUserInput={setUserInput}
-                            setShowModal={setShowModal}
-                        />
-                    }
-                />
+                <Route path="/overview" element={<OverviewTab />} />
+                <Route path="/wallet" element={<WalletTab />} />
                 <Route path="/profile" element={<ProfileTab />} />
                 <Route path="/settings/*" element={<SettingsTab />} />
                 <Route path="/help-center" element={<HelpCenterTab />} />
