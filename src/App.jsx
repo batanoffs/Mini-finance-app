4b20a9bdda922624c79173feffdec66b24f5e7ec
batanoffs <batanoff.s@protonmail.com>
2025-01-12 23:42:33 +0200

    move auth provider to main

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,24 @@ import { Routes, Route } from 'react-router-dom'
 import { App } from 'antd'
 
 import { Dashboard, Footer, Header, Home, Register, Login, About } from './pages/index'
-import { AuthProvider } from './contexts/AuthContext'
 import { PageNotFound } from './components/utils/404'
 
 const MyApp = () => {
     return (
-        <AuthProvider>
-            <App style={{ fontFamily: 'var(--body-font-family)' }}>
-                <Header />
-                <main>
-                    <Routes>
-                        <Route path="*" element={<PageNotFound />} />
-                        <Route path="/" element={<Home />} />
-                        <Route path="/about" element={<About />} />
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/register/*" element={<Register />} />
-                        <Route path="/dashboard/*" element={<Dashboard />} />
-                    </Routes>
-                </main>
-                <Footer />
-            </App>
-        </AuthProvider>
+        <App style={{ fontFamily: 'var(--body-font-family)' }}>
+            <Header />
+            <main>
+                <Routes>
+                    <Route path="*" element={<PageNotFound />} />
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register/*" element={<Register />} />
+                    <Route path="/dashboard/*" element={<Dashboard />} />
+                </Routes>
+            </main>
+            <Footer />
+        </App>
     )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import { StrictMode } from 'react'
+import { AuthProvider } from './contexts/AuthContext'
 import MyApp from './App'
 
 import './css/site.css'
@@ -8,7 +9,9 @@ import './css/site.css'
 createRoot(document.getElementById('root')).render(
     <BrowserRouter basename="/">
         <StrictMode>
-            <MyApp />
+            <AuthProvider>
+                <MyApp />
+            </AuthProvider>
         </StrictMode>
     </BrowserRouter>
 )
