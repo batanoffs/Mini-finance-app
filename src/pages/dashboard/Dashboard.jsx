89b6400d87fe5dd229212673a866b827923fb047
batanoffs <batanoff.s@protonmail.com>
2025-01-13 22:14:45 +0200

    update component secondary routes to be passes by outlet

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,21 +1,12 @@
-import { Route, Routes } from 'react-router-dom'
-import { HelpCenterTab, OverviewTab, ProfileTab, UserSettingsTab, Upgrade, WalletTab, Sidebar } from './assets/index'
-
-import styles from './dashboard-layout.module.css'
+import { Outlet } from 'react-router-dom'
+import { Sidebar } from './assets/index'
 
 export const Dashboard = () => {
     return (
         <div className="main-wrapper">
             <Sidebar />
 
-            <Routes>
-                <Route path="/overview" element={<OverviewTab styles={styles} />} />
-                <Route path="/wallet" element={<WalletTab styles={styles} />} />
-                <Route path="/profile" element={<ProfileTab styles={styles} />} />
-                <Route path="/settings/*" element={<UserSettingsTab styles={styles} />} />
-                <Route path="/help-center" element={<HelpCenterTab styles={styles} />} />
-                <Route path="/upgrade" element={<Upgrade styles={styles} />} />
-            </Routes>
+            <Outlet />
         </div>
     )
 }
