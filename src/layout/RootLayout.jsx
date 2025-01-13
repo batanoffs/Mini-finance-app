ae35326912c5da82a4b13ad1d58e647d06a5d41f
batanoffs <batanoff.s@protonmail.com>
2025-01-13 22:14:12 +0200

    add base root layout component to wrap the application

---
+++ src/layout/RootLayout.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from 'react-router-dom'
+import { Footer, Header } from '../pages'
+
+export const RootLayout = () => {
+    return (
+        <>
+            <Header />
+            <main>
+                <Outlet />
+            </main>
+            <Footer />
+        </>
+    )
+}
