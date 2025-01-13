25fa2211d152723c808105de15528e7b53bde899
batanoffs <batanoff.s@protonmail.com>
2025-01-13 22:11:42 +0200

    add session storage util

---
+++ src/utils/sessionStorage.js
@@ -0,0 +1,21 @@
+export const getUserData = () => {
+    const data = sessionStorage.getItem('auth')
+    const token = sessionStorage.getItem('token')
+
+    return {
+        data: data ? JSON.parse(data) : undefined,
+        token: token ? token : undefined,
+    }
+}
+
+export const setUserData = (data) => {
+    sessionStorage.setItem('auth', JSON.stringify(data))
+}
+
+export const clearUserData = () => {
+    sessionStorage.removeItem('auth')
+}
+
+//TODO - add more functions for token validation, etc.
+
+// clearUserToken
