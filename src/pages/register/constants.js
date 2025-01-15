b0f86a8c268eb58cde327f283ffbf7940d6cf830
batanoffs <batanoff.s@protonmail.com>
2025-01-15 06:55:56 +0200

    add initial form state constants for registration

---
+++ src/pages/register/constants.js
@@ -0,0 +1,14 @@
+export const formInitialState = {
+    email: '',
+    password: '',
+    confirmPassword: '',
+    firstName: '',
+    lastName: '',
+    gender: '',
+    country: '',
+    phoneNumber: '',
+    cardId: 0,
+    address: '',
+    town: '',
+    termsAccept: false,
+};
