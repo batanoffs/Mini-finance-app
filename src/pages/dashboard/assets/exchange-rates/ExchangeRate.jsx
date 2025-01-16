dca6ff6fc22e957ff33e11f811a0c757c670c1b5
batanoffs <batanoff.s@protonmail.com>
2025-01-16 21:18:44 +0200

    refactor ExchangeRate component; replace inline styles with CSS module for improved styling and maintainability

--- src/pages/dashboard/assets/exchange-rates/ExchangeRate.jsx
+++ src/pages/dashboard/assets/exchange-rates/ExchangeRate.jsx
@@ -2,13 +2,15 @@ import { useExchangeRates } from '../../../../hooks';
 import { ListRate } from '../../../../components/lists';
 import { EmptyCard } from '../../../../components/cards';
 
+import styles from './exchange-rate.module.css';
+
 export const ExchangeRate = () => {
     const rates = useExchangeRates();
     const mapRates = Array.from(Object.entries(rates)).map((rate) => ListRate(rate));
 
     return (
         <EmptyCard title="Exchange Rate">
-            <ul style={{ display: 'flex', flexDirection: 'column' }}>{mapRates}</ul>
+            <ul className={styles.list}>{mapRates}</ul>
         </EmptyCard>
     );
 };
