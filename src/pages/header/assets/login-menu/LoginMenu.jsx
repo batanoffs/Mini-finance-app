1b6d0183526cfcd4eb86b5f36c0c8b8b5dfffb77
batanoffs <batanoff.s@protonmail.com>
2025-01-16 19:10:32 +0200

    refactor login buttons; replace LoginButtons component with LoginMenu for improved mobile usability and organization

---
+++ src/pages/header/assets/login-menu/LoginMenu.jsx
@@ -0,0 +1,48 @@
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+import styles from './LoginMenu.module.css';
+
+export const LoginMenu = () => {
+    return (
+        <div className={styles.loginMenuContainer}>
+            <div
+                to="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+                className={styles.dropdownToggle}
+            >
+                <div className={`${styles.headerMobileDropdown} ${styles.headerButtons}`}>
+                    <FontAwesomeIcon className={styles.headerDropdownIcon} icon={faBars} />
+                </div>
+            </div>
+            <ul className={styles.dropdownMenu}>
+                <li>
+                    <Link
+                        to="/login"
+                        className={`${styles.buttonLogin} ${styles.dropdownItem}`}
+                        type="button"
+                        style={{
+                            borderBottomRightRadius: '0px',
+                            borderTopRightRadius: '0px',
+                        }}
+                    >
+                        Log in
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        to="/register"
+                        className={`${styles.buttonRegister} ${styles.dropdownItem}`}
+                        name="register"
+                        type="button"
+                    >
+                        New Account
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    );
+};
