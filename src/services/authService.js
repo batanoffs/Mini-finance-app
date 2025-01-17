9e2631994833e535234896899cc7e5b61f55a660
batanoffs <batanoff.s@protonmail.com>
2025-01-17 02:01:16 +0200

    add API constants and update authService; add reset password endpoint and improve code consistency

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,3 +1,4 @@
+import { encodeURIComponent } from 'querystring';
 import { API } from '../constants/baseUrl'
 import * as request from './requester'
 
@@ -13,8 +14,14 @@ const register = async (data) => {
     return await request.post(API.REGISTER, data)
 }
 
+const resetPassword = async (email) => {
+    const encodedEmail = encodeURIComponent(email);
+    return await request.get(API.RESET_PASSWORD(encodedEmail));
+}
+
 export const authService = {
     login,
     register,
     logout,
+    resetPassword
 }
