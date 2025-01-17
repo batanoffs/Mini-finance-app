0c118a82e141db4ca3bbf76ca6b2317dbebac6d3
batanoffs <batanoff.s@protonmail.com>
2025-01-17 20:30:10 +0200

    refactor: improve formatting and structure in useVirtualCard hook for better readability

--- src/hooks/useVirtualCard.js
+++ src/hooks/useVirtualCard.js
@@ -1,27 +1,27 @@
-import { useCallback, useContext, useState, useEffect } from 'react'
-import { AuthContext } from '../contexts/AuthContext'
-import { transactionService } from '../services/transactionService'
+import { useCallback, useContext, useState, useEffect } from 'react';
+import { AuthContext } from '../contexts/AuthContext';
+import { transactionService } from '../services/transactionService';
 
 export const useVirtualCard = () => {
-    const {  token, auth, setAuth } = useContext(AuthContext)
-    const [card, setCard] = useState(auth.virtualCard)
+    const { token, auth, setAuth } = useContext(AuthContext);
+    const [card, setCard] = useState(auth.virtualCard);
 
     const fetchBalance = useCallback(() => {
         transactionService
             .updateBalance(auth.objectId, card.objectId, token)
             .then((data) => {
-                setCard({ ...card, balance: data.results.updateMoney.result.balance })
+                setCard({ ...card, balance: data.results.updateMoney.result.balance });
                 setAuth({
                     ...auth,
                     virtualCard: { ...card, balance: data.results.updateMoney.result.balance },
-                })
+                });
             })
-            .catch((err) => console.log('Error during fetch card balance', err.message))
-    }, [])
+            .catch((err) => console.log('Error during fetch card balance', err.message));
+    }, []);
 
     useEffect(() => {
-        fetchBalance()
-    }, [fetchBalance])
+        fetchBalance();
+    }, [fetchBalance]);
 
-    return [card, auth]
-}
+    return [card, auth];
+};
