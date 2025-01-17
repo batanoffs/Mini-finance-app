df57e94b6e05c148c3570e906393a3956ae69c27
batanoffs <batanoff.s@protonmail.com>
2025-01-17 20:21:12 +0200

    move to archive folder

---
+++ src/utils/archive/assignNewCardId.js
@@ -0,0 +1,18 @@
+import { cardService } from '../../services'
+
+export const assignNewCardId = async () => {
+    let newCardId
+    let checkMatch = true
+    try {
+        while (checkMatch) {
+            newCardId = Math.floor(Math.random() * 100) + 1
+            const getUsersCardIds = await cardService.getVirtualCardIds()
+            if (getUsersCardIds && getUsersCardIds.message) return getUsersCardIds
+            const usersCardIds = getUsersCardIds.map((id) => id.cardId)
+            checkMatch = usersCardIds.includes(newCardId)
+        }
+        return newCardId
+    } catch (error) {
+        return error
+    }
+}
