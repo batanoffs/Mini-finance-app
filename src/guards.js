49c6cfb83a4d40c575dc1157420551444241fecb
batanoffs <batanoff.s@protonmail.com>
2025-01-19 01:28:48 +0200

    refactor: enhance guards to utilize getUserToken for improved authentication checks and streamline code structure

--- src/guards.js
+++ src/guards.js
@@ -1,41 +1,43 @@
-import { redirect } from 'react-router-dom'
+import { redirect } from 'react-router-dom';
 
-import { clearUserData, getUserData } from './utils/sessionStorage'
+import { clearUserData, getUserData, getUserToken } from './utils/sessionStorage';
 
 // restrict the login page if the user is already logged in
 export const restrictLoginPage = () => {
-    const { data, token } = getUserData()
+    const { data } = getUserData();
+    const { token } = getUserToken();
 
     // if there is missing data in the local storage, clear the data
     if (!data || !token) {
-        clearUserData()
+        clearUserData();
     }
     // if the user is logged in, redirect to the home page
     else {
-        return redirect('/dashboard')
+        return redirect('/dashboard');
     }
 
-    return null
-}
+    return null;
+};
 
 export const restrictDashboard = ({ request }) => {
-    const { data, token } = getUserData()
+    const { data } = getUserData();
+    const { token } = getUserToken();
 
     // if there is missing data in the local storage, redirect to the home page
     if (!data) {
-        return redirect('/')
+        return redirect('/');
     }
 
     // if there is missing token or is_admin data, clear the data and redirect to the login page
     if (!token) {
-        clearUserData()
-        return redirect('/login')
+        clearUserData();
+        return redirect('/login');
     }
 
     // if the user is not an admin and tries to access the register page, redirect to the keywords page
     if (request.url.includes('/register')) {
-        return redirect('/dashboard/overview')
+        return redirect('/dashboard/overview');
     }
 
-    return data
-}
+    return data;
+};
