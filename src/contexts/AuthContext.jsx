fae176b7537b1202d806f529884decd289a1cb66
batanoffs <batanoff.s@protonmail.com>
2025-01-19 01:32:08 +0200

    refactor: improve AuthContext by utilizing getUserToken for token retrieval and enhancing code clarity

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,43 +1,41 @@
-import { useContext, createContext } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useContext, createContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import { useSessionStorage } from '../hooks/useSessionStorage'
-import { authService } from '../services/authService'
-import { clearUserData } from '../utils/sessionStorage'
-import { DEFAULT_VALUES } from '../constants'
+import { clearUserData, getUserToken } from '../utils';
+import { DEFAULT_VALUES } from '../constants';
+import { useSessionStorage } from '../hooks';
+import { authService } from '../services';
 
-export const AuthContext = createContext()
+export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    const [auth, setAuth] = useSessionStorage(`auth`, DEFAULT_VALUES)
-    const navigate = useNavigate()
+    const [auth, setAuth] = useSessionStorage(`auth`, DEFAULT_VALUES);
+    const navigate = useNavigate();
 
     const onLogoutHandler = async () => {
         try {
-            const token = sessionStorage.getItem('token')
-            await authService.logout(token)
+            const { token } = getUserToken();
 
-            clearUserData()
-            setAuth({})
-            navigate('/')
-            
+            await authService.logout(token);
+
+            clearUserData();
+            setAuth({});
+            navigate('/');
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
-    }
+    };
 
     const authDataContext = {
         onLogoutHandler,
-        isAuthenticated: () => sessionStorage.getItem('token'),
-        token: sessionStorage.getItem('token') || 'No information',
         setAuth,
         auth,
-    }
+    };
 
-    return <AuthContext.Provider value={{ ...authDataContext }}>{children}</AuthContext.Provider>
-}
+    return <AuthContext.Provider value={{ ...authDataContext }}>{children}</AuthContext.Provider>;
+};
 
 export const useAuthContext = () => {
-    const context = useContext(AuthContext)
-    return context
-}
+    const context = useContext(AuthContext);
+    return context;
+};
