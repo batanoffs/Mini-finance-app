fe23af986758f60291db92bfd94dff167cf98f8a
batanoffs <batanoff.s@protonmail.com>
2025-01-18 22:58:56 +0200

    refactor: update EmptyCard component to handle null title and conditionally render header

--- src/components/cards/empty/EmptyCard.jsx
+++ src/components/cards/empty/EmptyCard.jsx
@@ -1,9 +1,9 @@
-import styles from './empty-card.module.css'
+import styles from './empty-card.module.css';
 
 export const EmptyCard = ({
     children,
     className = '',
-    title = '',
+    title = null,
     color = 'primary',
     options = {
         menu: null,
@@ -13,16 +13,28 @@ export const EmptyCard = ({
         primary: styles.primary,
         secondary: styles.secondary,
         accent: styles.accent,
-    }[color]
+    }[color];
+
+    let TitleItem = null;
+
+    if (title || options.menu) {
+        TitleItem = (
+            <header className={styles.header}>
+                {title && <h5>{title}</h5>}
+                {options.menu && typeof options.menu === 'string' ? (
+                    <span>{options.menu}</span>
+                ) : (
+                    options.menu
+                )}
+            </header>
+        );
+    }
 
     return (
         <section className={`${styles.customBlock} ${backgroundColor} ${className}`}>
-            <header className={styles.header}>
-                <h5>{title}</h5>
-                {options.menu && typeof options.menu === 'string' ? <span>{options.menu}</span> : options.menu}
-            </header>
+            {TitleItem}
 
             {children && <article className={styles.content}>{children}</article>}
         </section>
-    )
-}
+    );
+};
