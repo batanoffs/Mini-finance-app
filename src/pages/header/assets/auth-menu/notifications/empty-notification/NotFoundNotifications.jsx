4670eace4b4ede4d53a5d7f20a6eb945e4cc32d5
batanoffs <batanoff.s@protonmail.com>
2025-01-21 12:18:51 +0200

    refactor: remove unused notification components and styles

--- src/pages/header/assets/auth-menu/notifications/empty-notification/NotFoundNotifications.jsx
+++
@@ -1,7 +0,0 @@
-export const NotFoundNotifications = () => {
-    return (
-        <li key="empty" className="notifications-block border-bottom">
-            <small>No notifications</small>
-        </li>
-    )
-}
--- src/pages/header/assets/auth-menu/notifications/index.js
+++
@@ -1,4 +0,0 @@
-export * from './friend-request/FriendRequestNotification';
-export * from './income/IncomeNotification';
-export * from './money-request/MoneyRequestNotification';
-export * from './empty-notification/NotFoundNotifications';
