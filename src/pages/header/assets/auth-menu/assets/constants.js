b591061d696a78b68674e4751f3b240e37c4bb0c
batanoffs <batanoff.s@protonmail.com>
2025-01-21 12:18:12 +0200

    refactor: update notification constants and relocate file path

---
+++ src/pages/header/assets/auth-menu/assets/constants.js
@@ -0,0 +1,17 @@
+const type = {
+    frRequest: 'friend request',
+    frAccept: 'friend accept',
+    cashReceived: 'money received',
+    cashRequest: 'money request',
+};
+
+const status = {
+    pending: 'pending',
+    accepted: 'accepted',
+    declined: 'declined',
+};
+
+export const NOTIFY = {
+    STATUS: status,
+    TYPE: type,
+};
