777eb7ae2b7ccfd5e5f1cee7a6d27769b8086c2f
batanoffs <batanoff.s@protonmail.com>
2025-01-23 23:07:52 +0200

    update file path folder; moved useEffect to useNotification hook, improved props drilling and styles optimization

--- src/pages/header/assets/auth-menu/assets/notifications/Notifications.jsx
+++ src/pages/header/assets/auth-menu/assets/notifications/Notifications.jsx
@@ -1,39 +1,24 @@
-import { useContext, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBell } from '@fortawesome/free-regular-svg-icons';
 
 import { NotificationItem } from './notification-item/NotificationItem';
 import { Empty } from './empty-notification/Empty';
-import { AuthContext } from '../../../../../contexts/AuthContext';
-import { notificationService } from '../../../../../services';
 import { useNotification } from './useNotification';
-import { getUserToken } from '../../../../../utils';
 
 import styles from './notifications.module.css';
 
 export const Notifications = () => {
-    const { auth } = useContext(AuthContext);
-    const { token } = getUserToken();
     const {
         notifications,
-        setNotifications,
         onDelete,
         onCashApprove,
         onCashDecline,
         onFriendAccept,
         onFriendReject,
-    } = useNotification(auth, []);
-
-    useEffect(() => {
-        notificationService
-            .getNotSeenNotifications(auth.ownerId, token)
-            .then((result) => setNotifications(result))
-            .catch((error) => console.log(error));
-    }, []);
+    } = useNotification([]);
 
     return (
         <div className={styles.dropdownNotifications}>
-
             {notifications?.length > 0 && <span className={styles.notificationDot} />}
 
             <FontAwesomeIcon icon={faBell} className={styles.notificationsIcon} />
@@ -44,7 +29,6 @@ export const Notifications = () => {
                         <NotificationItem
                             key={notification.objectId}
                             notification={notification}
-                            ownerId={auth.ownerId}
                             onFriendReject={onFriendReject}
                             onFriendAccept={onFriendAccept}
                             onCashDecline={onCashDecline}
