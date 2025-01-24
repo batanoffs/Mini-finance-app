b8a5e62ec69a1f834758989d7bd031f8cea18fc7
batanoffs <batanoff.s@protonmail.com>
2025-01-24 19:28:52 +0200

    refactor useAddFriend hook; rename variables for clarity and improve error handling; clean up

--- src/hooks/useAddFriends.js
+++ src/hooks/useAddFriends.js
@@ -13,74 +13,61 @@ export const useAddFriend = () => {
     const onFriendRequest = async (formData) => {
         try {
             // Get the target user's phone number
-            const friendNumber = formData.phone;
+            const phone = formData.phone;
 
             // Check if the user provided a phone number
-            if (!friendNumber) throw new Error('No phone number provided');
+            if (!phone) throw new Error('No phone number provided');
 
             // Check if the user is trying to add themselves
-            if (friendNumber === auth.phoneNumber) throw new Error('You cannot add yourself');
+            if (phone === auth.phoneNumber) throw new Error('You cannot add yourself');
 
-            // Verify the friend request
-            const receiverUserId = await verifyFriendRequest(friendNumber);
-
-            // Send the friend request
-            if (receiverUserId) {
-                const response = await notificationService.createNotification(
-                    telephone,
-                    null,
-                    'friend request',
-                    receiverUserId,
-                    token
-                );
-
-                // Validate the response
-                if (!response?.success) throw new Error('Failed to send friend request');
-
-                // Show success message
-                showMessage('success', 'Friend request sent');
-            }
-        } catch (error) {
-            // Handle the error
-            console.error(error);
-            showMessage('error', error.message || 'Failed to send friend request');
-        }
-    };
-
-    const verifyFriendRequest = useCallback(
-        async (telephone) => {
             // Get the receiver's user data
-            const userData = await dataService.getAttribute('phoneNumber', telephone);
+            const friend = await dataService.getUserDataByAttribute('phoneNumber', phone);
 
             // Check if the user exists
-            if (!userData || userData.length === 0)
-                throw new Error('User with this phone number does not exist');
+            if (!friend || friend.length === 0) throw new Error('User with this phone number does not exist');
 
             // Get the receiver's id
-            const receiverUserId = userData[0].objectId;
+            const friendId = friend[0].objectId;
 
-            // Check if the user is already friend with the receiver
-            const validateFriend = auth.friends?.some(
-                (friend) => friend.objectId === receiverUserId
-            );
-            if (validateFriend) {
-                showMessage('warning', 'User is already your friend');
-                return null;
-            }
+            // Check if the user is already your friend
+            const validateFriend = auth.friends?.some((friend) => friend.objectId === friendId);
+
+            // Validate if the user is already a friend
+            if (validateFriend) throw new Error('This user is already your friend');
+
+            // TODO - could be improved just by query logic to the backend
             // Get all friend requests notifications
             const userFriendRequests = await notificationService.getAllFriendRequests(token);
 
             // Find if friend request has been sent before
             const filterFriendRequests = userFriendRequests?.filter(
                 ({ receiver, sender }) =>
-                    (receiver?.[0].objectId === receiverUserId &&
-                        sender[0].objectId === auth.ownerId) ||
-                    (receiver?.[0].objectId === auth.ownerId &&
-                        sender[0].objectId === receiverUserId)
+                    (receiver?.[0].objectId === friendId && sender[0]?.objectId === auth.ownerId) 
+                ||
+                    (receiver?.[0].objectId === auth.ownerId && sender[0]?.objectId === friendId)
             );
 
-            // If no friend request has been sent before, return the receiver's userId
-            if (filterFriendRequests?.length === 0) return receiverUserId;
+            // TODO -------------------------------------------------------
+
+            // If no friend request has been sent before, send the request
+            if (filterFriendRequests?.length === 0) {
+
+                // Send the friend request
+                const response = await notificationService.createNotification(
+                    friendId,
+                    'friend request',
+                    auth.objectId,
+                    token
+                );
+
+                // Validate the response
+                if (!response?.success)
+                    throw new Error(response.message || 'Failed to send friend request');
+
+                // Show success message
+                showMessage('success', 'Friend request sent');
+            }
 
             const status = filterFriendRequests[0]?.status;
 
@@ -93,9 +80,13 @@ export const useAddFriend = () => {
                 // Flip the request to the sender
                 return await handleDeclinedRequest(filterFriendRequests);
             }
-        },
-        [auth]
-    );
+
+        } catch (error) {
+            // Handle the error
+            console.error(error.message || error);
+            showMessage('error', 'Failed to send friend request');
+        }
+    };
 
     // Handle the declined request
     const handleDeclinedRequest = useCallback(
@@ -140,138 +131,3 @@ export const useAddFriend = () => {
         onFriendRequest,
     };
 };
-
-// const checkExistingNotification = useCallback(
-//     async (receiverId) => {
-//         const notifications = await notificationService.getAllFriendRequests(token);
-//         return notifications?.filter(
-//             (request) =>
-//                 (request.receiver?.[0].objectId === receiverId &&
-//                     request.sender[0].objectId === auth.ownerId) ||
-//                 (request.receiver?.[0].objectId === auth.ownerId &&
-//                     request.sender[0].objectId === receiverId)
-//         );
-//     },
-//     [auth.ownerId, token]
-// );
-
-// const onSubmit = async (e) => {
-//     e.preventDefault()
-//     try {
-//         if (!number) {
-//             throw new Error('No phone number')
-//         }
-//         if (number === phone) {
-//             throw new Error('You cannot add yourself')
-//         }
-
-//         const findReceiver = await dataService.getAttribute('phoneNumber', number)
-//         if (!findReceiver || findReceiver.length === 0) {
-//             throw new Error('User with this phone number does not exist')
-//         }
-
-//         const receiver = findReceiver[0].objectId
-//         const findFriend = friends?.some((friend) => friend.objectId === receiver)
-
-//         if (findFriend) {
-//             throw new Error('User is already your friend')
-//         }
-//         // GET NOTIFICATIONS AND FILTER FOR SENDER RECEIVER
-//         const allFriendRequestNotifications = await notificationService.getAllFriendRequests(
-//             token
-//         )
-//         if (!allFriendRequestNotifications) {
-//             throw new Error('Failed to fetch friend request notifications')
-//         }
-//         const checkFriendNotification = allFriendRequestNotifications.filter(
-//             (request) =>
-//                 (request.receiver?.length &&
-//                     request.receiver[0].objectId === receiver &&
-//                     request.sender[0].objectId === userDataId) ||
-//                 (request.receiver?.length &&
-//                     request.receiver[0].objectId === userDataId &&
-//                     request.sender[0].objectId === receiver)
-//         )
-//         //CHECK IF NOTIFICATION EXIST
-//         if (checkFriendNotification.length === 0) {
-//             const response = await notificationService.createNotification(
-//                 number,
-//                 null,
-//                 'friend request',
-//                 userDataId,
-//                 token
-//             )
-
-//             if (!response || !response.success) {
-//                 throw new Error('Failed to send friend request')
-//             }
-//             showMessage('success', 'Friend request sent')
-//         } else {
-//             const senderID = checkFriendNotification[0]?.sender[0]?.objectId
-//             const receiverID = checkFriendNotification[0]?.receiver[0]?.objectId
-//             const notificationId = checkFriendNotification[0].objectId
-//             const status = checkFriendNotification[0]?.status
-
-//             if (status === 'pending') new Error('You have already sent a friend request')
-//             if (status === 'accepted') new Error('You are already friends')
-//             if (status === 'declined') {
-//                 // CHECK IF FILTERED NOTIFICATION SENDER's ID IS CURRENT USER ID OR NOT
-//                 // UPDATE RELATIONS to flip the friend request to the other user
-//                 if (receiverID === userDataId) {
-//                     const setSenderResponse = await notificationService.updateRelation(
-//                         notificationId,
-//                         'sender',
-//                         receiverID,
-//                         token
-//                     )
-//                     const setReceiverResponse = await notificationService.updateRelation(
-//                         notificationId,
-//                         'receiver',
-//                         senderID,
-//                         token
-//                     )
-
-//                     console.log('setSenderResponse', setSenderResponse)
-//                     console.log('setReceiverResponse', setReceiverResponse)
-
-//                     if (
-//                         !setSenderResponse ||
-//                         !setReceiverResponse ||
-//                         setSenderResponse === 0 ||
-//                         setReceiverResponse === 0
-//                     ) {
-//                         throw new Error('Failed to update relation')
-//                     }
-//                     const response = await notificationService.updateNotificationStatus(
-//                         notificationId,
-//                         'pending',
-//                         false,
-//                         token
-//                     )
-//                     if (!response) {
-//                         throw new Error('Error accepting friend request')
-//                     }
-//                     showMessage('success', 'Friend request sent')
-//                 }
-
-//                 // ELSE ONLY CHANGE THE STATUS FOR SEEN AND STATUS TO PENDING
-//                 if (senderID === userDataId) {
-//                     const response = await notificationService.updateNotificationStatus(
-//                         notificationId,
-//                         'pending',
-//                         false,
-//                         token
-//                     )
-//                     if (!response || !response.success) {
-//                         throw new Error('Error accepting friend request')
-//                     }
-//                     showMessage('success', 'Friend request sent')
-//                 }
-//             }
-//         }
-//     } catch (error) {
-//         console.error(error)
-//         showMessage('error', error.message)
-//     }
-//     setNumber('')
-// }
