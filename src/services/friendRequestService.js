66fbaecbb13835b5554360f3c6babd9b5b46cd9b
batanoffs <batanoff.s@protonmail.com>
2025-01-27 05:16:11 +0200

    refactor friendRequestService to use Backendless API for creating friend requests and notifications; streamline transaction handling

--- src/services/friendRequestService.js
+++ src/services/friendRequestService.js
@@ -1,65 +1,107 @@
 import * as request from '../utils/requester';
 import { API } from '../constants/apiKeys';
 
-const getById = async (id) => {
-    const response = request.get(API.data.friendRequests + `/${id}`);
-    return response.json();
-};
-
 const create = async (friendPhone, currentUserId, token) => {
-    const body = {
-        isolationLevelEnum: 'READ_COMMITTED',
-        operations: [
-            {
-                operationType: 'FIND',
-                table: 'user-data',
-                opResultId: 'findFriend',
-                payload: {
-                    whereClause: `phoneNumber = '${friendPhone}'`,
-                },
-            },
-            {
-                operationType: 'CREATE',
-                table: 'friend-requests',
-                opResultId: 'newFriendRequest',
-            },
-            {
-                operationType: 'ADD_RELATION',
-                table: 'friend-requests',
-                opResultId: 'setReceiver',
-                payload: {
-                    parentObject: {
-                        ___ref: true,
-                        opResultId: 'newFriendRequest',
-                        propName: 'receiverId',
-                    },
-                    unconditional: {
-                        ___ref: true,
-                        opResultId: 'findFriend',
-                    },
-                },
-            },
-            {
-                operationType: 'ADD_RELATION',
-                table: 'friend-requests',
-                opResultId: 'setSender',
-                payload: {
-                    parentObject: {
-                        ___ref: true,
-                        opResultId: 'newFriendRequest',
-                        propName: 'objectId',
-                    },
-                    relationColumn: 'senderId',
-                    unconditional: [currentUserId],
-                },
-            },
-        ],
-    };
+    // Beginning of the transaction
+    const unitOfWork = new Backendless.UnitOfWork();
 
-    return await request.post(API.data.friendRequests, body, null, token);
+    // Create a query builder
+    const queryBuilder = Backendless.DataQueryBuilder.create(token);
+
+    // Build the query
+    queryBuilder.setWhereClause(`phoneNumber = '${friendPhone}'`);
+    queryBuilder.getProperties(['objectId']);
+
+    // Find the user
+    const findUserResult = unitOfWork.find('user-data', queryBuilder);
+
+    // Resolve the query
+    const findFriend = findUserResult.resolveTo(0);
+
+    // Create a new friend request entry
+    const newFriendRequest = unitOfWork.create('friend-requests', { status: 'pending' });
+
+    // Set the receiver relation
+    unitOfWork.setRelation(newFriendRequest, 'receiverId', findFriend);
+
+    // Set the sender relation
+    unitOfWork.setRelation(newFriendRequest, 'senderId', [currentUserId]);
+
+    // Create notification
+    const notification = unitOfWork.create('notifications', {
+        type: 'friend-request',
+        message: `${auth.fullName} sent you a friend request`,
+    });
+
+    // Set the receiver relation
+    unitOfWork.setRelation(notification, 'userId', findFriend);
+
+    // End of the transaction
+    return unitOfWork.execute(token);
+
+    // const body = {
+    //     isolationLevelEnum:
+    //         'REPEATABLE_READ' | 'READ_COMMITTED' | 'READ_UNCOMMITTED' | 'SERIALZABLE',
+    //     operations: [
+    //         {
+    //             operationType: 'FIND',
+    //             table: 'user-data',
+    //             opResultId: 'findFriend',
+    //             payload: {
+    //                 properties: ['objectId'],
+    //                 whereClause: `phoneNumber = '${friendPhone}'`,
+    //             },
+    //         },
+    //         {
+    //             operationType: 'CREATE',
+    //             table: 'friend-requests',
+    //             opResultId: 'newFriendRequest',
+    //             payload: {
+    //                 status: 'pending',
+    //             },
+    //         },
+    //         {
+    //             operationType: 'SET_RELATION',
+    //             table: 'friend-requests',
+    //             opResultId: 'addReceiver',
+    //             payload: {
+    //                 parentObject: {
+    //                     ___ref: true,
+    //                     opResultId: 'newFriendRequest',
+    //                     propName: 'objectId',
+    //                 },
+
+    //                 relationColumn: 'receiverId',
+    //                 unconditional: [
+    //                     {
+    //                         ___ref: true,
+    //                         opResultId: 'findFriend',
+    //                         resultIndex: 0,
+    //                         propName: 'objectId',
+    //                     },
+    //                 ],
+    //             },
+    //         },
+    //         {
+    //             operationType: 'SET_RELATION',
+    //             table: 'friend-requests',
+    //             opResultId: 'addSender',
+    //             payload: {
+    //                 parentObject: {
+    //                     ___ref: true,
+    //                     opResultId: 'newFriendRequest',
+    //                     propName: 'objectId',
+    //                 },
+    //                 relationColumn: 'senderId',
+    //                 unconditional: [currentUserId],
+    //             },
+    //         },
+    //     ],
+    // };
+
+    // return await request.post(API.transaction, body, null, token);
 };
 
 export const friendRequestService = {
-    getById,
     create,
 };
