065b85d8d24ba1decd30a552066366cee57eae03
batanoffs <batanoff.s@protonmail.com>
2025-01-27 05:07:00 +0200

    add Backendless initialization configuration and integrate it in main entry point

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
 import { ConfigProvider } from 'antd';
 
+import initializeBackendless from './config/backendless';
 import App from './App';
 
 import './css/site.css';
+
+initializeBackendless();
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
