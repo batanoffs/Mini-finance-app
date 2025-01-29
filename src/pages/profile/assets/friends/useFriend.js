9c0352ad0d63533c733f73e54b09dec035ed257e
batanoffs <batanoff.s@protonmail.com>
2025-01-29 14:24:42 +0200

    refactor useFriend hook to work according new updated db and sdk introduction

--- src/pages/profile/assets/friends/useFriend.js
+++ src/pages/profile/assets/friends/useFriend.js
@@ -1,93 +1,74 @@
-import { useCallback, useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useMemo, useState } from 'react';
 
 import { AuthContext } from '../../../../contexts/AuthContext';
-import { dataService, notificationService } from '../../../../services';
+import { dataService } from '../../../../services';
 import { useMessage } from '../../../../hooks';
 import { getUserToken } from '../../../../utils';
 
 export const useFriends = () => {
     const { auth, setAuth } = useContext(AuthContext);
-    const [friends, setFriends] = useState(auth.friends);
+    const [allFriends, setAllFriends] = useState(auth.friends);
     const [search, setSearch] = useState('');
     const { token } = getUserToken();
     const showMessage = useMessage();
 
-    // Fetch current user's friends
-    const fetchFriends = useCallback(async () => {
-        const userData = await dataService.getUserData(auth.objectId, ['friends']);
-        setFriends(userData[0].friends);
-    }, [auth.ownerId]);
+    const activeFriends = useMemo(() => {
+        if (!search) return allFriends;
+        return allFriends.filter((friend) =>
+            friend.fullName.toLowerCase().includes(search.toLowerCase())
+        );
+    }, [allFriends, search]);
+    
 
     useEffect(() => {
-        fetchFriends();
+        dataService
+            .getUserData(auth.objectId, ['friends'])
+            .then((userData) => {
+                setAllFriends(userData.friends);
+                setAuth({ ...auth, friends: userData.friends });
+            })
+            .catch((error) => console.error(error));
     }, []);
 
-    const onRemoveFriend = async (e) => {
+    const onRemoveFriend = async (friendId) => {
         try {
-            // Get the friend's id
-            const friendId = e.currentTarget.parentElement.parentElement.getAttribute('data-key');
+            // Check if the friend's id is available
             if (!friendId) throw new Error('Friend not found');
 
-            // Get all friend request notifications
-            const frRequestNotifications = await notificationService.getAllFriendRequests(token);
-            if (!frRequestNotifications)
-                throw new Error('Failed to fetch friend request notifications');
-
-            // Check if the friend request notification exists
-            const checkNotification = frRequestNotifications.filter((request) => {
-                const receiverId = request.receiver[0].objectId;
-                const senderId = request.sender[0].objectId;
-                return (
-                    (request.receiver?.length &&
-                        receiverId === friendId &&
-                        senderId === auth.objectId) ||
-                    (request.receiver?.length &&
-                        receiverId === auth.objectId &&
-                        senderId === friendId)
-                );
-            });
-
-            // If the notification exists, delete it
-            await notificationService.deleteNotification(checkNotification[0].objectId);
-
             // Remove the friend from the user's friend list
-            await dataService.removeRelation(auth.objectId, 'friends', [friendId]);
+            const response = await dataService.removeFriend(auth.objectId, friendId, token);
 
-            // Remove the user from the friend's friend list
-            await dataService.removeRelation(friendId, 'friends', [auth.objectId]);
+            // Check for errors
+            if (!response.success) throw new Error(response.error.message);
 
-            // Update the user's friend list
+            // Update session storage
             const filterFriends = auth.friends.filter((friend) => friend.objectId !== friendId);
-
-            // Update the user's friend list in the context and session storage
-            setAuth({ ...auth, friends: filterFriends || [] });
             sessionStorage.setItem('auth', JSON.stringify({ ...auth, friends: filterFriends }));
+
+            // Update the state
+            setAuth({ ...auth, friends: filterFriends || [] });
+
+            // Update the filtered friends list
+            setAllFriends(filterFriends);
 
             // Show success message
             showMessage('success', 'Friend removed successfully');
         } catch (error) {
-            showMessage('error', error.message);
             console.error(error);
+            showMessage('error', error.message || 'An error occurred');
         }
     };
 
     const onSearch = (e) => {
         const value = e.target.value;
         setSearch(value);
-
-        if (!value === '') {
-            const filteredList = friends.filter((friend) =>
-                friend.fullName.toLowerCase().includes(value.toLowerCase())
-            );
-            setFriends(filteredList);
-        }
     };
 
     return {
-        friends,
+        friends: activeFriends,
         search,
         onSearch,
-        setFriends,
+        setFriends: setAllFriends,
         onRemoveFriend,
     };
 };
