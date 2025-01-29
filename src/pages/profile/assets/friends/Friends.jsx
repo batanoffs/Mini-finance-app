fa37ca47e754bcc3402de517bfc8e1fe0a75b083
batanoffs <batanoff.s@protonmail.com>
2025-01-29 14:23:08 +0200

    refactor Friends component for improved accessibility and structure; add prop types validation and enhance search functionality

--- src/pages/profile/assets/friends/Friends.jsx
+++ src/pages/profile/assets/friends/Friends.jsx
@@ -1,6 +1,7 @@
 import { Empty } from 'antd';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import PropTypes from 'prop-types';
 
 import { FormInput } from '../../../../components/inputs';
 import { FriendItem } from './FriendItem';
@@ -15,27 +16,49 @@ export const Friends = () => {
         <div className={`${styles.customBlock} ${styles.customBlockProfile}`}>
             <h5>Friends</h5>
             <div className={styles.searchWrapper}>
-                <FontAwesomeIcon className={styles.searchIcon} icon={faSearch} />
+                <FontAwesomeIcon className={styles.searchIcon} icon={faSearch} aria-hidden="true" />
                 <FormInput
                     className={styles.searchInput}
                     placeholder="Search friend"
-                    id="Search friend"
+                    id="friend-search"
                     type="text"
                     autoComplete="off"
                     onChange={onSearch}
                     value={search}
+                    aria-label="Search friends"
                 />
             </div>
-            <ul className={styles.friendsList}>
-                {friends?.length === 0 ? (
+            <ul className={styles.friendsList} role="list">
+                {friends && friends.length > 0 ? (
+                    friends.map((friend) => (
+                        <FriendItem 
+                            key={friend.objectId}
+                            friend={friend} 
+                            onRemoveFriend={onRemoveFriend}
+                        />
+                    ))
+                ) : (
                     <Empty
                         className={styles.empty}
                         description="No results found. Add friends to find them here."
                     />
-                ) : (
-                    <FriendItem friends={friends} onRemoveFriend={onRemoveFriend} />
                 )}
             </ul>
         </div>
     );
+};
+
+Friends.propTypes = {
+    friends: PropTypes.arrayOf(
+        PropTypes.shape({
+            objectId: PropTypes.string.isRequired,
+            fullName: PropTypes.string.isRequired,
+            avatar: PropTypes.string,
+            country: PropTypes.string,
+            phoneNumber: PropTypes.string
+        })
+    ),
+    search: PropTypes.string,
+    onSearch: PropTypes.func.isRequired,
+    onRemoveFriend: PropTypes.func.isRequired
 };
