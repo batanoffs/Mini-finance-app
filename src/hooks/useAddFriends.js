d3126dd1b1460e8668129488dc128e5bb59c32bc
batanoffs <batanoff.s@protonmail.com>
2025-01-31 16:43:08 +0200

    refactor useAddFriend hook; enhance friend request validation and error handling; improve user feedback on request status

--- src/hooks/useAddFriends.js
+++ src/hooks/useAddFriends.js
@@ -10,28 +10,43 @@ export const useAddFriend = () => {
 
     const onFriendRequest = async (formData) => {
         try {
+
             // Get the user's phone number and the friend's phone number
             const friendsPhoneNumber = formData.phone;
             const authUserId = auth.objectId;
+            const currentUserName = auth.fullName;
             const userPhoneNumber = auth.phoneNumber;
 
             // Check if the user provided a phone number
             if (!friendsPhoneNumber) throw new Error('No phone number provided');
 
+            if (!auth || !auth.objectId || !auth.phoneNumber)
+                throw new Error('User not authenticated');
+
             // Check if the user is trying to add themselves
             if (friendsPhoneNumber === userPhoneNumber) throw new Error('You cannot add yourself');
 
             // Check if already friends
-            const response = await dataService.checkUserFriend(authUserId, friendsPhoneNumber);
+            // const response = await dataService.validateFriendExist(authUserId, friendsPhoneNumber);
+            // if (response && response.length > 0) throw new Error(response.error.message || 'This user is already your friend');
+            const isYourFriend = auth.friends.find((x) => x.phoneNumber === friendsPhoneNumber);
 
-            // Validate the response
-            if (response.length > 0)
-                throw new Error(response.error.message || 'This user is already your friend');
+            if (isYourFriend) throw new Error('This user is already your friend');
 
-            // Create the friend request
+            // Find the friend by phone number
+            const friend = await dataService.getByAttr(friendsPhoneNumber);
+
+            // If already friends
+            if (!friend[0].objectId) throw new Error('User not found');
+
+            //TODO - check if the same friend request exists in the database
+
+
+            // Create the friend request and notification
             const createResponse = await friendRequestService.create(
-                friendsPhoneNumber,
+                friend[0].objectId,
                 authUserId,
+                currentUserName,
                 token
             );
 
@@ -40,6 +55,7 @@ export const useAddFriend = () => {
                 throw new Error(createResponse.error.message || 'Failed to send friend request');
 
             // Show a success message
+            formData.phone = '';
             showMessage('success', 'Friend request sent');
         } catch (error) {
             // Handle the error
