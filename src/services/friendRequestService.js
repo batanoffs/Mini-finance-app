56c93668a8942915a08e44041c74313756c023b8
batanoffs <batanoff.s@protonmail.com>
2025-01-31 16:41:01 +0200

    refactor friendRequestService; enhance friend request creation and notification handling

--- src/services/friendRequestService.js
+++ src/services/friendRequestService.js
@@ -1,45 +1,33 @@
-const create = async (friendPhone, currentUserId, token) => {
+const create = async (friendId, currentUserId, currentUserName, token) => {
     try {
         // Beginning of the transaction
         const unitOfWork = new Backendless.UnitOfWork();
 
-        // Create a query builder
-        const queryBuilder = Backendless.DataQueryBuilder.create(token);
+        // Create a friend request
+        const friendRequest = unitOfWork.create('friend-requests', {
+            status: 'pending',
+        });
 
-        // Build the query
-        queryBuilder.setWhereClause(`phoneNumber = '${friendPhone}'`);
-        queryBuilder.getProperties(['objectId']);
-
-        // Find the user
-        const findUserResult = unitOfWork.find('user-data', queryBuilder);
-
-        // Resolve the query
-        const findFriend = findUserResult.resolveTo(0);
-
-        // Create a new friend request entry
-        const newFriendRequest = unitOfWork.create('friend-requests', { status: 'pending' });
-
-        // Set the receiver relation
-        unitOfWork.setRelation(newFriendRequest, 'receiverId', findFriend);
-
-        // Set the sender relation
-        unitOfWork.setRelation(newFriendRequest, 'senderId', [currentUserId]);
-
-        // Create notification
+        // Create notification with the direct reference to the operation result
         const notification = unitOfWork.create('notifications', {
             type: 'friend-request',
-            message: `${auth.fullName} sent you a friend request`,
-            related_entity_id: newFriendRequest.objectId,
+            message: `${currentUserName} sent you a friend request`,
+            related_entity_id: friendRequest,
             related_entity_name: 'friend-requests',
         });
 
+        // Set the relations after object creation
+        unitOfWork.setRelation(friendRequest, 'receiverId', [friendId]);
+        unitOfWork.setRelation(friendRequest, 'senderId', [currentUserId]);
+
         // Set the receiver relation
-        unitOfWork.setRelation(notification, 'userId', findFriend);
+        unitOfWork.setRelation(notification, 'userId', [friendId]);
 
         // End of the transaction
         return unitOfWork.execute(token);
     } catch (error) {
-        throw new Error(error);
+        console.log('error', error);
+        throw error;
     }
 };
 
