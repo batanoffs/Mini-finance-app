223ce445cb84916a8132f91a69ee8a2f7686c2e6
batanoffs <batanoff.s@protonmail.com>
2025-02-01 08:55:55 +0200

    refactor FriendItem component to use ButtonTooltip for actions and remove unused styles

--- src/pages/profile/assets/friends/FriendItem.jsx
+++ src/pages/profile/assets/friends/FriendItem.jsx
@@ -1,8 +1,8 @@
 import { faMoneyBill, faPiggyBank, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import PropTypes from 'prop-types';
 
 import styles from './friend-item.module.css';
+import { ButtonTooltip } from '../../../../components/buttons';
 
 export const FriendItem = ({ friend, onRemoveFriend }) => {
     const handleRemoveFriend = () => {
@@ -12,52 +12,49 @@ export const FriendItem = ({ friend, onRemoveFriend }) => {
     const formatPhoneNumber = (phone) => {
         return phone.replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3');
     };
-    
+
     return (
         <li className={styles.entryWrapper}>
-            <img 
-                src={friend.avatar} 
-                className={styles.profileImage} 
+            <img
+                src={friend.avatar}
+                className={styles.profileImage}
                 alt={`${friend.fullName}'s avatar`}
             />
 
             <div className={styles.friendInfo}>
                 <strong>{friend.fullName}</strong>
                 <p>{friend.country}</p>
-                <a 
+                <a
                     href={`tel:${friend.phoneNumber}`}
-                    aria-label={`Call ${friend.fullName} at ${formatPhoneNumber(friend.phoneNumber)}`}
+                    aria-label={`Call ${friend.fullName} at ${formatPhoneNumber(
+                        friend.phoneNumber
+                    )}`}
                 >
                     {formatPhoneNumber(friend.phoneNumber)}
                 </a>
             </div>
 
             <div className={styles.friendButtons} role="group" aria-label="Friend actions">
-                <button 
-                    type="button"
-                    className={styles.friendButton} 
-                    aria-label={`Request money from ${friend.fullName}`}
-                    data-text="Request money"
-                >
-                    <FontAwesomeIcon className={styles.icon} icon={faMoneyBill} aria-hidden="true" />
-                </button>
-                <button 
-                    type="button"
-                    className={styles.friendButton} 
-                    aria-label={`Send money to ${friend.fullName}`}
-                    data-text="Send money"
-                >
-                    <FontAwesomeIcon className={styles.icon} icon={faPiggyBank} aria-hidden="true" />
-                </button>
-                <button
-                    type="button"
-                    className={styles.friendButton}
-                    aria-label={`Remove ${friend.fullName} from friends`}
-                    data-text="Remove friend"
+                <ButtonTooltip
+                    size='small'
+                    type="request"
+                    text="Request money"
+                    icon={faMoneyBill}
+                    onClick={() => {}}
+                />
+                <ButtonTooltip
+                    size='small'
+                    type="send"
+                    text="Send money"
+                    icon={faPiggyBank}
+                    onClick={() => {}}
+                />
+                <ButtonTooltip
+                    size='small'
+                    text="Remove friend"
+                    icon={faTrashAlt}
                     onClick={handleRemoveFriend}
-                >
-                    <FontAwesomeIcon className={styles.icon} icon={faTrashAlt} aria-hidden="true" />
-                </button>
+                />
             </div>
         </li>
     );
@@ -69,7 +66,7 @@ FriendItem.propTypes = {
         fullName: PropTypes.string.isRequired,
         avatar: PropTypes.string.isRequired,
         country: PropTypes.string.isRequired,
-        phoneNumber: PropTypes.string.isRequired
+        phoneNumber: PropTypes.string.isRequired,
     }).isRequired,
-    onRemoveFriend: PropTypes.func.isRequired
+    onRemoveFriend: PropTypes.func.isRequired,
 };
