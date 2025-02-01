70dbf6b1d0d39d6f2063257a02f274c421ac9d82
batanoffs <batanoff.s@protonmail.com>
2025-02-01 08:55:25 +0200

    update import

--- src/pages/dashboard/assets/banking-actions/BankingActions.jsx
+++ src/pages/dashboard/assets/banking-actions/BankingActions.jsx
@@ -1,7 +1,7 @@
 import { faVault, faMoneyBillTransfer, faHandHoldingDollar } from '@fortawesome/free-solid-svg-icons'
 
 import { TransactionsModal, TopUp } from '../../../../components/modals'
-import { BankingButton } from '../../../../components/buttons'
+import { ButtonTooltip } from '../../../../components/buttons'
 import { EmptyCard } from '../../../../components/cards'
 import { useModal } from '../../../../hooks'
 
@@ -17,9 +17,9 @@ export const BankingActions = () => {
     return (
         <EmptyCard title="Banking Actions" color="primary" className={styles.customBlockBottom}>
             <div className={styles.transactionBtns}>
-                <BankingButton text="Top up account" type="topUp" icon={faVault} onClick={toggleModal} />
-                <BankingButton text="Send money" type="send" icon={faMoneyBillTransfer} onClick={toggleModal} />
-                <BankingButton text="Request money" type="request" icon={faHandHoldingDollar} onClick={toggleModal} />
+                <ButtonTooltip text="Top up account" type="topUp" icon={faVault} onClick={toggleModal} />
+                <ButtonTooltip text="Send money" type="send" icon={faMoneyBillTransfer} onClick={toggleModal} />
+                <ButtonTooltip text="Request money" type="request" icon={faHandHoldingDollar} onClick={toggleModal} />
             </div>
 
             {showModal.topUp && <TopUp toggleModal={toggleModal} />}
