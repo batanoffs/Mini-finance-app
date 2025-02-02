918d6f1af4b49d52b3b4af97764db6f8b5cca297
batanoffs <batanoff.s@protonmail.com>
2025-02-02 18:41:36 +0200

    rename initializeBackendless to initializeBackendlessSDK for clarity

--- src/config/backendless.js
+++ src/config/backendless.js
@@ -1,8 +1,7 @@
 import Backendless from 'backendless';
 
 // Initialize Backendless with environment variables
-export default function initializeBackendless() {
-    // TODO - update for production
+export default function initializeBackendlessSDK() {
     Backendless.serverURL = import.meta.env.VITE_API_BASE_URL;
     Backendless.initApp(import.meta.env.VITE_APP_ID, import.meta.env.VITE_API_KEY);
-};
+}
