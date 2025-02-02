12e0e49a381ddb0f5154859a4c1140e5402a0d4a
batanoffs <batanoff.s@protonmail.com>
2025-02-02 18:43:34 +0200

    refactor RootLayout to use Ant Design App component and add custom styles

--- src/layout/root/RootLayout.jsx
+++ src/layout/root/RootLayout.jsx
@@ -1,15 +1,18 @@
 import { Outlet } from 'react-router-dom';
+import { App } from 'antd';
 
 import { Footer, Header } from '../../pages';
 
+import styles from './root-layout.module.css';
+
 export const RootLayout = () => {
     return (
-        <>
+        <App className={styles.antLayout}>
             <Header />
             <main>
                 <Outlet />
             </main>
             <Footer />
-        </>
+        </App>
     );
 };
