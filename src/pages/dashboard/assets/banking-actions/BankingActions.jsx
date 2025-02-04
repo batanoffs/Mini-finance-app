fe52d9a0eca5ed608bcaf6fdd82c73bc3ce95a3b
batanoffs <batanoff.s@protonmail.com>
2025-02-04 08:19:33 +0200

    updated according to changes in modal component

--- src/pages/dashboard/assets/banking-actions/BankingActions.jsx
+++ src/pages/dashboard/assets/banking-actions/BankingActions.jsx
@@ -1,30 +1,72 @@
-import { faVault, faMoneyBillTransfer, faHandHoldingDollar } from '@fortawesome/free-solid-svg-icons'
+import {
+    faVault,
+    faMoneyBillTransfer,
+    faHandHoldingDollar,
+} from '@fortawesome/free-solid-svg-icons';
 
-import { TransactionsModal, TopUp } from '../../../../components/modals'
-import { ButtonTooltip } from '../../../../components/buttons'
-import { EmptyCard } from '../../../../components/cards'
-import { useModal } from '../../../../hooks'
+import { TransactionsModal, TopUp } from '../../../../components/modals';
+import { ButtonTooltip } from '../../../../components/buttons';
+import { EmptyCard } from '../../../../components/cards';
+import { useMessage, useModal } from '../../../../hooks';
+import { useAuthContext } from '../../../../contexts/AuthContext';
+import { getUserToken } from '../../../../utils';
 
-import styles from './banking-actions.module.css'
+import styles from './banking-actions.module.css';
 
 export const BankingActions = () => {
-    const [showModal, toggleModal] = useModal({
+    const [showModal, setShowModal] = useModal({
         topUp: false,
         send: false,
         request: false,
-    })
+    });
+    const { auth } = useAuthContext();
+    const { token } = getUserToken();
+    const showMessage = useMessage();
 
     return (
         <EmptyCard title="Banking Actions" color="primary" className={styles.customBlockBottom}>
             <div className={styles.transactionBtns}>
-                <ButtonTooltip text="Top up account" type="topUp" icon={faVault} onClick={toggleModal} />
-                <ButtonTooltip text="Send money" type="send" icon={faMoneyBillTransfer} onClick={toggleModal} />
-                <ButtonTooltip text="Request money" type="request" icon={faHandHoldingDollar} onClick={toggleModal} />
+                <ButtonTooltip
+                    text="Top up account"
+                    type="topUp"
+                    icon={faVault}
+                    onClick={setShowModal}
+                />
+                <ButtonTooltip
+                    text="Send money"
+                    type="send"
+                    icon={faMoneyBillTransfer}
+                    onClick={setShowModal}
+                />
+                <ButtonTooltip
+                    text="Request money"
+                    type="request"
+                    icon={faHandHoldingDollar}
+                    onClick={setShowModal}
+                />
             </div>
 
-            {showModal.topUp && <TopUp toggleModal={toggleModal} />}
-            {showModal.send && <TransactionsModal.Send showModal={showModal} toggleModal={toggleModal} />}
-            {showModal.request && <TransactionsModal.Request showModal={showModal} toggleModal={toggleModal} />}
+            {showModal.topUp && <TopUp toggleModal={setShowModal} />}
+            {showModal.send && (
+                <TransactionsModal.Send
+                    showModal={showModal}
+                    setShowModal={setShowModal}
+                    userId={auth.objectId}
+                    userFullName={auth.fullName}
+                    token={token}
+                    showMessage={showMessage}
+                />
+            )}
+            {showModal.request && (
+                <TransactionsModal.Request
+                    showModal={showModal}
+                    setShowModal={setShowModal}
+                    userId={auth.objectId}
+                    userFullName={auth.fullName}
+                    token={token}
+                    showMessage={showMessage}
+                />
+            )}
         </EmptyCard>
-    )
-}
+    );
+};
