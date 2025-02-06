1b10f126930e4c3f1dc8a142ba8fda8b87d4ffe8
batanoffs <batanoff.s@protonmail.com>
2025-02-06 13:06:11 +0200

    refactor: simplify RenderMessage component and update notification message handling

--- src/pages/header/assets/auth-menu/assets/notifications/notification-item/RenderMessage.jsx
+++ src/pages/header/assets/auth-menu/assets/notifications/notification-item/RenderMessage.jsx
@@ -1,63 +1,19 @@
 import { useAuthContext } from '../../../../../../../contexts/AuthContext';
-import { NOTIFY } from '../constants';
+import { NOTIFICATION } from '../constants';
 
 import styles from './render-message.module.css';
 
-export const RenderMessage = ({ notification }) => {
-    const { auth } = useAuthContext();
+export const RenderMessage = ({
+    message,
+    related_entity_id,
+    related_entity_name,
+    userId,
+    created,
+    type,
+    is_seen,
+    objectId,
+}) => {
+    // <img className={styles.profileImage} src={avatar} alt="avatar" /> 
 
-    // Define variables for notification data
-    const senderName = notification.sender?.[0]?.fullName ?? 'Unknown';
-    const receiverId = notification?.receiver?.[0]?.objectId ?? null;
-    const event = notification.event_type;
-    const status = notification.status;
-    const seen = notification.seen;
-    const avatar = notification.sender?.[0]?.avatar;
-    const amount = notification.amount ?? 'Unknown';
-
-    // event === frRequest && receiverId === auth.ownerId && seen === false
-    const FriendAccept = <small>{senderName + 'has accepted your friend request'}</small>;
-
-    // event === frRequest && status === pending
-    const FriendRequest = (
-        <>
-            <img className={styles.profileImage} src={avatar} alt="avatar" />
-            <small>Friend request from {senderName} </small>
-        </>
-    );
-
-    // event === cashReceived
-    const CashIncome = (
-        <small>
-            Received <b style={{ color: 'green' }}>{amount}BGN</b> from {senderName}
-        </small>
-    );
-
-    // event === cashRequest
-    const CashRequest = (
-        <>
-            <img className={styles.profileImage} src={avatar} alt="avatar" />
-            <small>
-                {senderName} requested <b style={{ color: 'darkred' }}>{amount}BGN</b>
-            </small>
-        </>
-    );
-
-    if (event === NOTIFY.TYPE.frRequest && receiverId === auth.ownerId && seen === false) {
-        return FriendAccept;
-    }
-
-    if (event === NOTIFY.TYPE.frRequest && status === NOTIFY.STATUS.pending) {
-        return FriendRequest;
-    }
-
-    if (event === NOTIFY.TYPE.cashReceived) {
-        return CashIncome;
-    }
-
-    if (event === NOTIFY.TYPE.cashRequest) {
-        return CashRequest;
-    }
-
-    return <div> Error </div>;
+    return <small>{message}</small>;
 };
