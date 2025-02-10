cbed659cfddfa97e683f05a42db90514a753c48e
batanoffs <batanoff.s@protonmail.com>
2025-02-10 18:33:18 +0200

    feat: enhance QuickSendMoney component with friend selection and modal for sending money; improved code clarity and consistency

--- src/pages/dashboard/assets/quick-send-money/QuickSendMoney.jsx
+++ src/pages/dashboard/assets/quick-send-money/QuickSendMoney.jsx
@@ -1,69 +1,87 @@
 import { useState } from 'react';
 
-import { Actions, ActionsMenu, AddToFavorites } from './assets';
-import { useMessage, useModal } from '../../../../hooks';
+import { Actions, ActionsMenu, AddToFavorites, FriendItem, SendForm } from './components';
+import { useMessage } from '../../../../hooks';
 import { useAuthContext } from '../../../../contexts/AuthContext';
-
-import { ListFriend } from '../../../../components/lists';
 import { EmptyCard } from '../../../../components/cards';
-import { TransactionsModal } from '../../../../components/modals';
-import { getUserToken } from '../../../../utils';
+import { ModalForm } from '../../../../components/modals';
 
 import styles from './quick-send.module.css';
 
 export const QuickSendMoney = () => {
-    const [values, setValues] = useState({ amount: '', friends: '' });
-    const [showModal, setShowModal] = useModal({ send: false, buttons: false, favFriends: false });
+    // Modal state
+    const [isAddMode, setAddMode] = useState(false);
+    const [isEditMode, setEditMode] = useState(false);
+    const [isSendOpen, setSendOpen] = useState(false);
+
+    // Form state
+    const [receiver, setReceiver] = useState({ name: '', id: '', avatar: '' });
+
     const { auth } = useAuthContext();
-    const { token } = getUserToken();
     const showMessage = useMessage();
 
-    const notFound = (
-        <>
-            <p className={styles.notFound}>No friends added yet. Add friends below</p>
-            <AddToFavorites toggleModal={setShowModal} />
-        </>
-    );
-    const menu =
-        auth?.favorite_friends?.length > 0 ? <ActionsMenu toggleModal={setShowModal} /> : null;
-
-    const openSendMenu = (name) => {
-        if (!name) {
-            showMessage('error', 'Can not get friend name. Please try again, or refresh page');
+    const openSendMenu = (favoriteFriend) => {
+        if (!favoriteFriend.fullName || !favoriteFriend.objectId || !favoriteFriend.avatar) {
+            showMessage('error', 'Can not get friend data. Please try again, or refresh page');
             return;
         }
-        setShowModal('send');
-        setValues({ ...values, [`friends`]: name });
+
+        setSendOpen(true);
+        setReceiver({
+            name: favoriteFriend.fullName,
+            id: favoriteFriend.objectId,
+            avatar: favoriteFriend.avatar,
+        });
     };
 
+    const favoriteFriends = auth?.favorite_friends;
+
+    if (!favoriteFriends.length)
+        return (
+            <EmptyCard title="Quick Send" color="accent">
+                <p className={styles.notFound}>No friends added yet. Add friends below</p>
+                <AddToFavorites toggleModal={setAddMode} />
+            </EmptyCard>
+        );
+
     return (
-        <EmptyCard options={{ menu }} title="Quick Send" color="accent">
-            <ul className={styles.sendMoneyContainer}>
-                {auth?.favorite_friends?.length > 0
-                    ? auth.favorite_friends?.map((friend) => (
-                          <ListFriend
-                              friend={friend}
-                              onClick={() => openSendMenu(friend?.fullName)}
-                              key={friend.objectId}
-                          >
-                              {showModal.send && (
-                                  <Actions toggleModal={setShowModal} friend={friend} />
-                              )}
-                          </ListFriend>
-                      ))
-                    : notFound}
-                {showModal.favFriends && <AddToFavorites toggleModal={setShowModal} />}
-                {showModal.send && (
-                    <TransactionsModal.Send
-                        showModal={showModal}
-                        setShowModal={setShowModal}
-                        userId={auth.objectId}
-                        userFullName={auth.fullName}
-                        token={token}
-                        showMessage={showMessage}
-                    />
-                )}
-            </ul>
-        </EmptyCard>
+        <>
+            <EmptyCard
+                title="Quick Send"
+                color="accent"
+                overflow="visible"
+                options={{
+                    menu: (
+                        <ActionsMenu
+                            favoriteFriends={favoriteFriends}
+                            toggleEditMode={setEditMode}
+                            toggleAddMode={setAddMode}
+                        />
+                    ),
+                }}
+            >
+                <ul className={styles.sendMoneyContainer}>
+                    {favoriteFriends.map((friend) => (
+                        <FriendItem
+                            friend={friend || null}
+                            onClick={() => openSendMenu(friend)}
+                            key={friend.objectId}
+                        >
+                            {isEditMode && <Actions setEditMode={setEditMode} friend={friend} />}
+                        </FriendItem>
+                    ))}
+                </ul>
+                {isAddMode && <AddToFavorites setAddMode={setAddMode} />}
+            </EmptyCard>
+
+            <ModalForm isVisible={isSendOpen} setVisible={setSendOpen} title={'Quick Send Money'}>
+                <SendForm
+                    userId={auth.objectId}
+                    userFullName={auth.fullName}
+                    receiver={receiver}
+                    setShowModal={setSendOpen}
+                />
+            </ModalForm>
+        </>
     );
 };
