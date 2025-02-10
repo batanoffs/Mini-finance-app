955930925f37964c45e760af86f150da9fd2f468
batanoffs <batanoff.s@protonmail.com>
2025-02-10 19:42:46 +0200

    simplify TransactionsForm component layout; removed unnecessary elements for improved clarity

--- src/pages/dashboard/assets/banking-actions/components/transactions-form/TransactionsForm.jsx
+++ src/pages/dashboard/assets/banking-actions/components/transactions-form/TransactionsForm.jsx
@@ -8,7 +8,7 @@ import styles from './modal.module.css';
 const Form = ({ type, setShowModal, userId, userFullName, token, showMessage }) => {
     const [values, setValues] = useState({ amount: '', friends: '', selectedFriendId: '' });
 
-    const { setUserInputHandler, onTransaction, onClose, friendSuggestions } = useMakeTransactions(
+    const { setUserInputHandler, onTransaction, friendSuggestions } = useMakeTransactions(
         type,
         userId,
         userFullName,
@@ -19,56 +19,38 @@ const Form = ({ type, setShowModal, userId, userFullName, token, showMessage }) 
         setValues
     );
 
-    let title = '';
-
-    if (type === 'request') title = 'Request Money';
-    if (type === 'send') title = 'Send Money';
-    if (type === 'topUp') title = 'Top Up Account';
-
     const onTransactionSubmit = (e) => {
         e.preventDefault();
         onTransaction(values.friends, values.selectedFriendId, values.amount);
     };
 
-    const onCloseOutsideClick = (e) => {
-        if (e.target === e.currentTarget) onClose();
-    };
-
     return (
-        <div className={styles.background} onClick={onCloseOutsideClick}>
-            <div className={styles.container}>
-                <form onSubmit={onTransactionSubmit} className={styles.customForm}>
-                    <header>
-                        <h5>{title}</h5>
-                        <button onClick={onClose}>X</button>
-                    </header>
-                    <FormInput
-                        type="number"
-                        name="amount"
-                        id="amount"
-                        className={styles.formControl}
-                        value={values.amount}
-                        onChange={setUserInputHandler}
-                        placeholder="Enter amount"
-                        suffixText="BGN"
-                        required
-                    />
+        <form onSubmit={onTransactionSubmit} className={styles.customForm}>
+            <FormInput
+                type="number"
+                name="amount"
+                id="amount"
+                className={styles.formControl}
+                value={values.amount}
+                onChange={setUserInputHandler}
+                placeholder="Enter amount"
+                suffixText="BGN"
+                required
+            />
 
-                    <Autocomplete
-                        userInput={values}
-                        setUserInput={setValues}
-                        name="friends"
-                        placeholder="Start typing a name..."
-                        required
-                        suggestions={friendSuggestions || []}
-                    />
+            <Autocomplete
+                userInput={values}
+                setUserInput={setValues}
+                name="friends"
+                placeholder="Start typing a name..."
+                required
+                suggestions={friendSuggestions || []}
+            />
 
-                    <footer>
-                        <FormInput type="submit" value="Submit" />
-                    </footer>
-                </form>
-            </div>
-        </div>
+            <footer>
+                <FormInput type="submit" value="Submit" />
+            </footer>
+        </form>
     );
 };
 
