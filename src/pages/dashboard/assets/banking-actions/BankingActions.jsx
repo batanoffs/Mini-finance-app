e281f462c42e3014e97313626690627b3c45f170
batanoffs <batanoff.s@protonmail.com>
2025-02-10 19:42:11 +0200

    feat: refactor BankingActions component to use state for modal visibility; updated ButtonTooltip to toggle modals directly

--- src/pages/dashboard/assets/banking-actions/BankingActions.jsx
+++ src/pages/dashboard/assets/banking-actions/BankingActions.jsx
@@ -1,72 +1,84 @@
+import { useState } from 'react';
 import {
     faVault,
     faMoneyBillTransfer,
     faHandHoldingDollar,
 } from '@fortawesome/free-solid-svg-icons';
 
-import { TopUpFrom, TransactionsForm } from './components';
+import { TopUpForm, TransactionsForm } from './components';
 import { ButtonTooltip } from '../../../../components/buttons';
 import { EmptyCard } from '../../../../components/cards';
-import { useMessage, useModal } from '../../../../hooks';
+import { ModalForm } from '../../../../components/modals';
+import { useMessage } from '../../../../hooks';
 import { useAuthContext } from '../../../../contexts/AuthContext';
 import { getUserToken } from '../../../../utils';
 
 import styles from './banking-actions.module.css';
 
 export const BankingActions = () => {
-    const [showModal, setShowModal] = useModal({
-        topUp: false,
-        send: false,
-        request: false,
-    });
+    const [isTopUpOpen, setTopUpModal] = useState(false);
+    const [isSendOpen, setSendModal] = useState(false);
+    const [isRequestOpen, setRequestModal] = useState(false);
+
     const { auth } = useAuthContext();
     const { token } = getUserToken();
     const showMessage = useMessage();
 
     return (
-        <EmptyCard title="Banking Actions" color="primary" className={styles.customBlockBottom}>
-            <div className={styles.transactionBtns}>
-                <ButtonTooltip
-                    text="Top up account"
-                    type="topUp"
-                    icon={faVault}
-                    onClick={setShowModal}
-                />
-                <ButtonTooltip
-                    text="Send money"
-                    type="send"
-                    icon={faMoneyBillTransfer}
-                    onClick={setShowModal}
-                />
-                <ButtonTooltip
-                    text="Request money"
-                    type="request"
-                    icon={faHandHoldingDollar}
-                    onClick={setShowModal}
-                />
-            </div>
+        <>
+            <EmptyCard title="Banking Actions" color="primary" className={styles.customBlockBottom}>
+                <div className={styles.transactionBtns}>
+                    <ButtonTooltip
+                        text="Top up account"
+                        icon={faVault}
+                        toggleModal={setTopUpModal}
+                    />
+                    <ButtonTooltip
+                        text="Send money"
+                        icon={faMoneyBillTransfer}
+                        toggleModal={setSendModal}
+                    />
+                    <ButtonTooltip
+                        text="Request money"
+                        icon={faHandHoldingDollar}
+                        toggleModal={setRequestModal}
+                    />
+                </div>
+            </EmptyCard>
 
-            {showModal.topUp && <TopUpFrom toggleModal={setShowModal} />}
-            {showModal.send && (
+            <ModalForm
+                title={'Top up your account'}
+                isVisible={isTopUpOpen}
+                setVisible={setTopUpModal}
+            >
+                <TopUpForm toggleModal={setTopUpModal} />
+            </ModalForm>
+
+            <ModalForm title={'Send money'} isVisible={isSendOpen} setVisible={setSendModal}>
                 <TransactionsForm.Send
-                    showModal={showModal}
-                    setShowModal={setShowModal}
+                    showModal={isSendOpen}
+                    setShowModal={setSendModal}
                     userId={auth.objectId}
                     userFullName={auth.fullName}
                     token={token}
                     showMessage={showMessage}
                 />
-            )}
-            {showModal.request && (
+            </ModalForm>
+
+            <ModalForm
+                title={'Request money'}
+                isVisible={isRequestOpen}
+                setVisible={setRequestModal}
+            >
                 <TransactionsForm.Request
-                    showModal={showModal}
-                    setShowModal={setShowModal}
+                    showModal={isRequestOpen}
+                    setShowModal={setRequestModal}
                     userId={auth.objectId}
                     userFullName={auth.fullName}
                     token={token}
                     showMessage={showMessage}
                 />
-            )}
-        </EmptyCard>
+            </ModalForm>
+        </>
     );
 };
