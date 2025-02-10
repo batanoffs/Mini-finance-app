edaab7add9e633fa878e916e440b3bfd8f202c00
batanoffs <batanoff.s@protonmail.com>
2025-02-10 19:41:54 +0200

    feat: update ButtonTooltip component to toggle modal on click; removed unused onClick prop

--- src/components/buttons/button-tooltip/ButtonTooltip.jsx
+++ src/components/buttons/button-tooltip/ButtonTooltip.jsx
@@ -5,17 +5,20 @@ import styles from './button-tooltip.module.css';
 export const ButtonTooltip = ({
     text,
     icon,
-    onClick,
-    type = null,
+    toggleModal,
     size = 'medium',
     className = '',
     ...rest
 }) => {
+    const onToggleModal = () => {
+        toggleModal(true);
+    };
+
     return (
         <button
             className={`${styles.transactionBtn} ${styles[size]} ${className}`}
             data-text={text}
-            onClick={() => onClick(type)}
+            onClick={onToggleModal}
             {...rest}
         >
             <FontAwesomeIcon className={styles.icon} icon={icon} />
